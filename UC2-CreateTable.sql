f8842eec0ef07c9f8077849c46ba97b8240034f8
Amartya <amartyaanand21@live.com>
2020-11-02 00:37:05 +0530

    [Amartya] Add : Creating AddressBook table.

---
+++ UC2-CreateTable.sql
@@ -0,0 +1,10 @@
+create table AddressBook
+(
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    address varchar(50) not null,
+    city varchar(20),
+    state varchar(20),
+    zip int,
+    phone_number varchar(10) not null
+);
