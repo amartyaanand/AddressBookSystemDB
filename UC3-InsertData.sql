7d24aa6913fd0ea18513be5888d65c7415a4949d
Amartya <amartyaanand21@live.com>
2020-11-02 00:48:41 +0530

    [Amartya] Add : Inserting data into the table.

---
+++ UC3-InsertData.sql
@@ -0,0 +1,7 @@
+insert into AddressBook values
+('MS','Dhoni','1000 Michigan Ave.','Chicago','IL',60605,'3889233120'),
+('Shikhar','Dhawan','1000 Harbor Ave.','Seattle','WA',98116,'8332443923'),
+('Virat','Kohli','360 Delmar Blvd.','New York','NY',10011,'8832322103'),
+('Rohit','Sharma','100 5th Ave.','New York','NY',10012,'8043232213'),
+('Jasprit','Bumrah','Stout St.','Denver','CO',80204,'7884440432'),
+('Ravi','Jadeja','Stout St.','Denver','CO',56478,'9876543217')
